35e918057f94eb4b2d645390a0c64c0fd326237a
jimmyshultz <jimmyshultz3@gmail.com>
2023-06-25 21:16:25 -0500

    Reverting to 0e75601 to set the secret key in a way that will work for heroku as well

--- resume/settings.py
+++ resume/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-&tm_*q0!+$k=yla!*i@s&tvm-zmb2&!jye0a)u-c#5$&ryvidz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
